fa4302932068378a8d19eb7a521420975422ada3
siranjeevan <smartjeevith2k@gamil.com>
2025-05-22 15:27:46 +0530

    Initial Commit

---
+++ fbAuth/fbAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  fbAuthApp.swift
+//  fbAuth
+//
+//  Created by Jeevith  on 22/05/25.
+//
+
+import SwiftUI
+
+@main
+struct fbAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
